aa9eb48016b9a8f872cdfb2b96cf9bd0b466eae5
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-19 15:18:28 +0200

    TASK-0001 added firstArray

--- src/tablice/Main.java
+++ src/tablice/Main.java
@@ -2,6 +2,14 @@ package tablice;
 
 public class Main {
     public static void main(String[] args) {
+        //task 1
+        firstArray();
+    }
 
+    private static void firstArray() {
+        int[] array = {5, 10, 15, 20, 25};
+        for (int i : array) {
+            System.out.println(i);
+        }
     }
 }
