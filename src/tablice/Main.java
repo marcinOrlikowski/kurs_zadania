964c8db4fb3ca065639f05ea17d950e9dac33074
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-19 16:19:20 +0200

    TASK-0013 added stringToCharArray method

--- src/tablice/Main.java
+++ src/tablice/Main.java
@@ -2,6 +2,15 @@ package tablice;
 
 public class Main {
     public static void main(String[] args) {
+        //task 13
+        stringToCharArray();
+    }
 
+    private static void stringToCharArray() {
+        String text = "Ala ma kota";
+        char[] charArray = text.toCharArray();
+        for (char c : charArray) {
+            System.out.println(c);
+        }
     }
 }
