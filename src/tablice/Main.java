f09a627e6df412b2ca5d9c9677a52e2055f73044
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-19 15:27:01 +0200

    TASK-0003 added createAndFillArray method

--- src/tablice/Main.java
+++ src/tablice/Main.java
@@ -1,7 +1,28 @@
 package tablice;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        //task 3
+        createAndFillArray();
+    }
 
+    private static void createAndFillArray() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array");
+        int size = sc.nextInt();
+        int[] array = new int[size];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Enter " + (i + 1 + " value"));
+            array[i] = sc.nextInt();
+        }
+
+        System.out.println("Values in array:");
+
+        for (int i : array) {
+            System.out.println(i);
+        }
     }
 }
