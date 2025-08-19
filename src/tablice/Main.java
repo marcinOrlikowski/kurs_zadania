2706de3aedce2d28ac4cbc7ab91f3560cf0fe982
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-19 15:37:17 +0200

    TASK-0005 added arithmeticAverage method

--- src/tablice/Main.java
+++ src/tablice/Main.java
@@ -2,6 +2,18 @@ package tablice;
 
 public class Main {
     public static void main(String[] args) {
+        //task 5
+        arithmeticAverage();
+    }
+
+    private static void arithmeticAverage() {
+        int[] array = {5, 10, 15, 20, 25};
+        int sum = 0;
+        for (int i : array) {
+            sum += i;
+        }
+        int average = sum / array.length;
+        System.out.printf("Average: %d", average);
 
     }
 }
