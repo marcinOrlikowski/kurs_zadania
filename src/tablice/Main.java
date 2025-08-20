fbf1ce7991ab39541b3fe3ffc81254479692490f
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-20 15:06:04 +0200

    TASK-0020 added matrixOperations method

--- src/tablice/Main.java
+++ src/tablice/Main.java
@@ -2,20 +2,26 @@ package tablice;
 
 public class Main {
     public static void main(String[] args) {
-        //task 19
-        diagonalInMatrix();
+        //task 20
+        matrixOperations();
     }
 
-    private static void diagonalInMatrix() {
+    private static void matrixOperations() {
         int[][] matrix = createAndFillMatrix();
-        for (int i = 0; i < matrix.length; i++) {
-            System.out.println(matrix[i][i]);
-        }
-
+        System.out.println("Before switching rows:");
+        printMatrix(matrix);
+        //switching rows
+        replaceFirstRowWithLastInMatrix(matrix);
+        System.out.println("After switching rows:");
+        printMatrix(matrix);
+        //switching diagonals
+        switchDiagonalsInMatrix(matrix);
+        System.out.println("After switching diagonals:");
+        printMatrix(matrix);
     }
 
     private static int[][] createAndFillMatrix() {
-        int[][] matrix = new int[10][10];
+        int[][] matrix = new int[5][5];
         int startingValue = 1;
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
@@ -24,4 +30,32 @@ public class Main {
         }
         return matrix;
     }
+
+    private static void replaceFirstRowWithLastInMatrix(int[][] matrix) {
+        int[] temp = matrix[0];
+        matrix[0] = matrix[matrix.length - 1];
+        matrix[matrix.length - 1] = temp;
+    }
+
+    private static void switchDiagonalsInMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i == j){
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[i][matrix[i].length - j - 1];
+                    matrix[i][matrix[j].length - j - 1] = temp;
+                }
+            }
+        }
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int[] ints : matrix) {
+            for (int i : ints) {
+                System.out.printf("%4d", i);
+            }
+            System.out.println();
+        }
+    }
 }
+
