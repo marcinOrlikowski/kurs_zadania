f43e882a294171f5b6e1c0bca94b6f63b16ec940
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-20 12:17:53 +0200

    TASK-0014 added matrix method

--- src/tablice/Main.java
+++ src/tablice/Main.java
@@ -1,7 +1,20 @@
 package tablice;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        //task 14
+        matrix();
+    }
 
+    private static void matrix() {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        for (int[] ints : matrix) {
+            System.out.println(Arrays.toString(ints));
+        }
     }
 }
