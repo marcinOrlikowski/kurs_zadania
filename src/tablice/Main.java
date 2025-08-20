f07dec8bf5311ec7be4e6e991fc2b164cdf5923b
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-20 13:14:47 +0200

    TASK-0017 added matrixSumOfColumns method

--- src/tablice/Main.java
+++ src/tablice/Main.java
@@ -3,26 +3,27 @@ package tablice;
 public class Main {
     public static void main(String[] args) {
         //task 14
-        matrixSumOfRows();
+        matrixSumOfColumns();
     }
 
-    private static void matrixSumOfRows() {
+    private static void matrixSumOfColumns() {
         int sum = 0;
         int[][] matrix = {
                 {4, 5, 6},
                 {7, 8, 9}
         };
 
-        for (int i = 0; i < matrix.length; i++) {
+        for (int j = 0; j < matrix[0].length; j++) {
             sum = 0;
-            for (int j = 0; j < matrix[i].length; j++) {
-                sum += matrix[i][j];
+            for (int[] ints : matrix) {
+                sum += ints[j];
             }
-            System.out.println("Sum in " + (i + 1) + " row is: " + sum);
+            System.out.println("Sum of " + (j + 1) + " column is: " + sum);
         }
-
     }
+
 }
+
 
 
 
