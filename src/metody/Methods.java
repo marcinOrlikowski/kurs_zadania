9ffb4f27fdefed2b5451886c8fb628fd973b1c6b
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-25 14:35:51 +0200

    methods/TASK-0010 added sumArray()

--- src/metody/Methods.java
+++ src/metody/Methods.java
@@ -2,7 +2,21 @@ package metody;
 
 public class Methods {
     public static void main(String[] args) {
-        
+        //task 10
+        int[] array = {5,10,15};
+        System.out.println("Sum in array is: " + sumArray(array));
+        /**
+         * Output:
+         * Sum in array is: 30
+         */
+    }
+
+    private static int sumArray(int[] array) {
+        int sum = 0;
+        for (int i : array) {
+            sum += i;
+        }
+        return sum;
     }
 
 }
