7ba667e8b128a9fdb7b810434b8104a03f84ab9e
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-25 13:15:21 +0200

    methods/TASK-0002 added mult()

--- src/metody/Methods.java
+++ src/metody/Methods.java
@@ -2,7 +2,13 @@ package metody;
 
 public class Methods {
     public static void main(String[] args) {
-        
+        //task 2
+        int result = mult(5, 10);
+        System.out.println(result);
+    }
+
+    private static int mult(int a, int b) {
+        return a * b;
     }
 
 }
