9740ee7716bac497644b7cb84a1e153754764f03
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-25 13:18:46 +0200

    methods/TASK-0003 added max()

--- src/metody/Methods.java
+++ src/metody/Methods.java
@@ -2,7 +2,17 @@ package metody;
 
 public class Methods {
     public static void main(String[] args) {
-        
+        //task 3
+        int result = max(9, 3);
+        System.out.println(result);
+    }
+
+    private static int max(int a, int b) {
+        if (a > b){
+            return a;
+        } else {
+            return b;
+        }
     }
 
 }
