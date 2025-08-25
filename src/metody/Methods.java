d71ebd44441a26f7710e33ff013e419f482be014
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-25 15:53:23 +0200

    method/TASK-0015 added sumDigitsIterative()

--- src/metody/Methods.java
+++ src/metody/Methods.java
@@ -2,7 +2,17 @@ package metody;
 
 public class Methods {
     public static void main(String[] args) {
-        
+        //task 15
+        System.out.println(sumDigitsIterative(12345));
+    }
+
+    private static int sumDigitsIterative(int n) {
+        int sum = 0;
+        while (n != 0){
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
     }
 
 }
