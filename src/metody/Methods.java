4c0256988faf97727f79b9a371361c18309586f5
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-25 16:19:12 +0200

    methods/TASK-0018 added gcdRecursive()

--- src/metody/Methods.java
+++ src/metody/Methods.java
@@ -2,7 +2,18 @@ package metody;
 
 public class Methods {
     public static void main(String[] args) {
-        
+        //task 18
+        System.out.println(gcdRecursive(21, 49));
+    }
+
+    private static int gcdRecursive(int a, int b) {
+        if (a == b) {
+            return a;
+        } else if (a > b){
+            return gcdRecursive(a-b,b);
+        } else {
+            return gcdRecursive(a,b-a);
+        }
     }
 
 }
