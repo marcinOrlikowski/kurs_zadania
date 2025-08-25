a384e5268649fd8abc3183f9d6b977d10918b894
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-25 16:00:29 +0200

    methods/TASK-0016 added sumDigitsRecursive()

--- src/metody/Methods.java
+++ src/metody/Methods.java
@@ -2,7 +2,17 @@ package metody;
 
 public class Methods {
     public static void main(String[] args) {
-        
+        //task 16
+        System.out.println(sumDigitsRecursive(12345));
+    }
+
+    private static int sumDigitsRecursive(int n) {
+        if (n == 0) {
+            return 0;
+        } else {
+            return (n % 10) + sumDigitsRecursive(n / 10);
+        }
+
     }
 
 }
