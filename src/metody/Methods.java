6ff26a86ca466a3dbcaa9584092528168cf78dbc
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-25 14:12:08 +0200

    methods/TASK-0007 added changeX()

--- src/metody/Methods.java
+++ src/metody/Methods.java
@@ -2,7 +2,20 @@ package metody;
 
 public class Methods {
     public static void main(String[] args) {
-        
+        //task 7
+        int x = 5;
+        changeX(x);
+        System.out.println("Value of 'x' in was not changed and still is: " + x);
+        /**
+         * Output:
+         * Value of 'x' changed locally in method: 10
+         * Value of 'x' in was not changed and still is: 5
+         */
+    }
+
+    private static void changeX(int x) {
+        x = 10;
+        System.out.println("Value of 'x' changed locally in method: " + x);
     }
 
 }
