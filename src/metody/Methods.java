e7d1ea6b9ed2739022fa2f52e0d2b5393dcbb2d2
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-25 14:31:12 +0200

    methods/TASK-0009 added factorialRecursive()

--- src/metody/Methods.java
+++ src/metody/Methods.java
@@ -2,8 +2,24 @@ package metody;
 
 public class Methods {
     public static void main(String[] args) {
-        
+        //task 9
+        System.out.println(factorialRecursive(4));
+        System.out.println(factorialRecursive(5));
+        System.out.println(factorialRecursive(10));
+        /**
+         * Output:
+         * 24
+         * 120
+         * 3628800
+         */
     }
 
+    private static int factorialRecursive(int n) {
+        if (n == 1 || n == 0) {
+            return 1;
+        } else {
+            return n * factorialRecursive(n - 1);
+        }
+    }
 }
 
