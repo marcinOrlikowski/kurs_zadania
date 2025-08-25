329e36d10c7d6e5459777d22dee9eaf40bf433e0
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-25 13:27:44 +0200

    methods/TASK-0005 added average()

--- src/metody/Methods.java
+++ src/metody/Methods.java
@@ -2,7 +2,17 @@ package metody;
 
 public class Methods {
     public static void main(String[] args) {
-        
+        //task 5
+        double result = average(5.0, 3.0, 4.0);
+        System.out.printf("Average: %.2f", result);
+        /**
+         * Output:
+         * Average: 4,00
+         */
+    }
+
+    private static double average(double a, double b, double c) {
+        return (a + b + c) / 3;
     }
 
 }
