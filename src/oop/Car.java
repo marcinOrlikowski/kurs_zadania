f1a07cb4cdc2751cb33a9dcb32aec100399cbfa4
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-29 14:37:49 +0200

    oop/TASK-0001 first class and object

---
+++ src/oop/Car.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class Car {
+    String brand;
+    int yearOfProduction;
+
+    @Override
+    public String toString() {
+        return "Brand: " + brand + ", year of production: " + yearOfProduction;
+    }
+}
--- src/oop/Main.java
+++ src/oop/Main.java
@@ -2,6 +2,9 @@ package oop;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Car car1 = new Car();
+        car1.brand = "Fiat";
+        car1.yearOfProduction = 2010;
+        System.out.println(car1);
     }
 }
