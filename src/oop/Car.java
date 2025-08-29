90665842a0b49af9a905ffdafaa5631c2117e66f
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-29 14:40:10 +0200

    oop/TASK-0002 No-args construcor

--- src/oop/Car.java
+++ src/oop/Car.java
@@ -4,6 +4,11 @@ public class Car {
     String brand;
     int yearOfProduction;
 
+    public Car() {
+        this.brand = "Unknown";
+        this.yearOfProduction = 2000;
+    }
+
     @Override
     public String toString() {
         return "Brand: " + brand + ", year of production: " + yearOfProduction;
--- src/oop/Main.java
+++ src/oop/Main.java
@@ -2,9 +2,14 @@ package oop;
 
 public class Main {
     public static void main(String[] args) {
+        //task 1
         Car car1 = new Car();
         car1.brand = "Fiat";
         car1.yearOfProduction = 2010;
         System.out.println(car1);
+
+        //task 2
+        Car car2 = new Car();
+        System.out.println(car2);
     }
 }
