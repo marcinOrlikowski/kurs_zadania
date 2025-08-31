2022ea9c606ee17cb7bf791c7fe1b729f895e94e
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-31 13:48:41 +0200

    enum/TASK-0001 simple anum - days of week

---
+++ src/enum_/DayOfWeek.java
@@ -0,0 +1,5 @@
+package enum_;
+
+public enum DayOfWeek {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
--- src/enum_/Main.java
+++ src/enum_/Main.java
@@ -2,6 +2,8 @@ package enum_;
 
 public class Main {
     public static void main(String[] args) {
-
+        //task 1
+        DayOfWeek friday = DayOfWeek.FRIDAY;
+        System.out.println(friday);
     }
 }
