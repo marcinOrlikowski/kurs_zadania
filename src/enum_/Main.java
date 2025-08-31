c5efb979b03ef7022920f2944356771da8aa264f
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-31 13:50:40 +0200

    enum/TASK-0002 enum and loop

--- src/enum_/Main.java
+++ src/enum_/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         //task 1
         DayOfWeek friday = DayOfWeek.FRIDAY;
-        System.out.println(friday);
+        System.out.println(friday + "\n");
+        //task 2
+        for (DayOfWeek day : DayOfWeek.values()) {
+            System.out.println(day);
+        }
     }
 }
