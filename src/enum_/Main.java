62a8d8a8d3b87537fd3413e9aed0b9014037efc4
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-31 13:54:12 +0200

    enum/TASK-0003 emum comparing

--- src/enum_/Main.java
+++ src/enum_/Main.java
@@ -2,12 +2,18 @@ package enum_;
 
 public class Main {
     public static void main(String[] args) {
-        //task 1
+        System.out.println("Task 1");
         DayOfWeek friday = DayOfWeek.FRIDAY;
-        System.out.println(friday + "\n");
-        //task 2
+        System.out.println(friday);
+
+        System.out.println("Task 2");
         for (DayOfWeek day : DayOfWeek.values()) {
             System.out.println(day);
         }
+
+        System.out.println("Task 3");
+        DayOfWeek monday = DayOfWeek.MONDAY;
+        DayOfWeek tuesday = DayOfWeek.TUESDAY;
+        System.out.println(monday == tuesday);
     }
 }
