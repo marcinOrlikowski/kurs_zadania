2a3e4eec0a49aeb6de9bb008bc9eabcf7a215f8d
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-09-02 15:23:06 +0200

    enum TASK-0014 formatting changes

--- src/enum_/OrderStatus.java
+++ src/enum_/OrderStatus.java
@@ -3,7 +3,7 @@ package enum_;
 public enum OrderStatus {
     NEW, PROCESSING, SHIPPED, DELIVERED, CANCELED;
 
-    public boolean isActive(){
+    public boolean isActive() {
         return this == NEW || this == PROCESSING || this == SHIPPED;
     }
 }
